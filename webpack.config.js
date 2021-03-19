21389d9b98ca947e489fbb9f1de1da09c9bc6918
Nicolasdha <sankp001@gmail.com>
2021-03-18 19:49:23 -0600

    Created ExpressJS server, and refactored webpack.config for production deloyment

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,40 @@
 
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
+// module.exports = {
+//     entry: './src/app.js',
+//     output: {
+//     path: path.join(__dirname, 'public'),
+//     filename: 'bundle.js'
+//     },
+//     module: {
+//         rules: [{
+//             loader: 'babel-loader',
+//             test: /\.js$/,
+//             exclude:/node_modules/
+//         }, {
+//             test: /\.s?css$/,
+//             use: [
+//                 'style-loader',
+//                 'css-loader',
+//                 'sass-loader'
+//             ]
+//         }]
+//     },
+//     // This is for source mapping 
+//     devtool: 'eval-cheap-module-source-map',
+//     devServer: {
+//         contentBase: path.join(__dirname, 'public')
+//     },
+// };
+
+module.exports = (env) =>{
+    console.log('env', env)
+
+    const isProduction = (env.production === true);
+
+    return {
+        entry: './src/app.js',
     output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
@@ -21,12 +53,14 @@ module.exports = {
             ]
         }]
     },
-    // This is for source mapping 
-    devtool: 'eval-cheap-module-source-map',
+    // This is for source mapping and dev-server
+    devtool: isProduction ? 'source-map' :'inline-source-map',
+    mode: isProduction ? 'production' : 'development',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
-    },
-};
-
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true,
+        }
+    }
+}
 
 
